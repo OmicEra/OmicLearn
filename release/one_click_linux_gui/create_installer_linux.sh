9cbf5fdeaaaa38ce92331881a80bdd0917e1a27f
furkanmtorun <furkanmtorun@gmail.com>
2023-05-05 21:14:59 +0200

    FIX Linux and Change Windows naming

--- release/one_click_linux_gui/create_installer_linux.sh
+++ release/one_click_linux_gui/create_installer_linux.sh
@@ -17,7 +17,7 @@ python setup.py sdist bdist_wheel
 # Setting up the local package
 cd release/one_click_linux_gui
 # Make sure you include the required extra packages and always use the stable or very-stable options!
-pip install "../../dist/OmicLearn-1.4-py3-none-any.whl"
+pip install "../../dist/omiclearn-1.3.1-py3-none-any.whl"
 
 # Creating the stand-alone pyinstaller folder
 pip install pyinstaller==4.10
@@ -30,7 +30,7 @@ conda deactivate
 
 # Wrapping the pyinstaller folder in a .deb package
 mkdir -p dist/omiclearn_gui_installer_linux/usr/local/bin
-mv dist/omiclearn dist/omiclearn_gui_installer_linux/usr/local/bin/omiclearn
+mv dist/OmicLearn dist/omiclearn_gui_installer_linux/usr/local/bin/OmicLearn
 mkdir dist/omiclearn_gui_installer_linux/DEBIAN
 cp control dist/omiclearn_gui_installer_linux/DEBIAN
 dpkg-deb --build --root-owner-group dist/omiclearn_gui_installer_linux/
